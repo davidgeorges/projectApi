cf98bad8e982eac01d144845696c818ac011040c
David <georges.david.jk@gmail.com>
2023-10-18 16:20:41 +0200

    Update model,database,main

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Date, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Date, Column, ForeignKey, Integer, String,Base
 from sqlalchemy.orm import relationship
 
 class User(Base):
