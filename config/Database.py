1a8134ec00f53ca90ef5088fddcc4b1ff6f5738d
David <georges.david.jk@gmail.com>
2023-10-19 11:03:46 +0200

    Delete SQLAlchemy

--- config/Database.py
+++ config/Database.py
@@ -1,16 +1,16 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-
+import mysql.connector
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL =  os.getenv("DATABASE_URL")
+# Configuration de la connexion MySQL
+config = {
+    "host": os.getenv("DATABASE_HOST"),
+    "user": os.getenv("DATABASE_USERNAME"),
+    "password": os.getenv("DATABASE_PASSWORD"),
+    "database": os.getenv("DATABASE_NAME")
+}
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+# Création de la connexion MySQL
+conn = mysql.connector.connect(**config)
+cursor = conn.cursor()
